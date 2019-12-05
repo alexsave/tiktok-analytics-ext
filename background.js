701a42f83ac5dc76ff16ecb191167925cb57c025
Alex Saveliev <alexvsaveliev@gmail.com>
2019-12-04 20:06:56 -0500

    added big comment on the format of the response

--- background.js
+++ background.js
@@ -1,3 +1,42 @@
+/*
+    The key intuition here is that by loading the page, TikTok reponds with a bunch of data
+    Reponses matching tiktok.com/share will respond with this format:
+    body:
+        hasMore: true,
+        itemListData: [
+            {
+                authorInfos: {
+                    nickName: "Rocket",
+                    uniqueId: "qzim",
+                    signature: "bio",
+                    userId: "5456789876545678"
+                },
+                challengeInfoList: {
+                    [
+                        {
+                            challengeName:"holidayhacks"
+                        },
+                        ...
+                    ]
+                },
+                itemInfos: {
+                    commentCount: 2,
+                    createTime: 15345678986, 
+                    diggCount: 2,
+                    id: "456787654",
+                    shareCount: 3,
+                    text: "yada yada yada"
+                    video: {urls: ["https://fdsafdsafds]}
+                },
+                musicInfos: {
+                    authorName: "brianna.caston",
+                    musicId: "4568765456",
+                    musicName: "New Hampshire Check"
+                },
+            },
+            ...
+        ]
+*/
 let map = {};
 
 function listener(details) {
@@ -19,7 +58,15 @@ function listener(details) {
   filter.onstop = event => {
     filter.disconnect();
     let obj = JSON.parse(data);
-    console.log(obj);
+    //console.log(obj.body);
+    
+    const items = obj.body.itemListData;
+    //each of these items has a itemInfos object that has
+    //comment 
+    //also has a autherInfos.uniqueId (ex. qzim) to be safe
+    items.forEach(item => 
+      console.log(item.itemInfos.text + ': ' + item.itemInfos.diggCount)
+    );
   }
 
   return {};
