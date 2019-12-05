18752b50d1df0366c055fcf88798fb506c6c4770
Alex Saveliev <alexvsaveliev@gmail.com>
2019-12-05 13:03:28 -0500

    simplified

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,24 @@
+let username = "";
+const sendMessage = () => {
+  console.log(input.value);
+  if (false)
+    browser.runtime.sendMessage({msg: 'start', username: 'qzim'});
+};
+
+const input = document.querySelector('input');
+input.onkeyup = e => {
+  if(e.keyCode === 13)
+    sendMessage()
+};
 /**
  * Listen for clicks on the buttons, and send the appropriate message to
  * the content script in the page.
  */
-document.addEventListener("click", (e) => {
+const button = document.querySelector('button');
+button.onclick = () => sendMessage();
+/*document.addEventListener("click", (e) => {
   //there's literally just one button
-  console.log('username');
+  console.log(input.value);
   if(false)
     browser.runtime.sendMessage({msg: 'start', username: 'qzim'});
-});
+});*/
