20943938eee80529692a7ce444b32ba28ed9afc5
Alex Saveliev <alexvsaveliev@gmail.com>
2019-12-04 22:00:13 -0500

    rearranged a few thigns

---
+++ popup/popup.js
@@ -0,0 +1,9 @@
+
+window.addEventListener("click", notifyExtension);
+
+function notifyExtension(e) {
+  if (e.target.tagName != "A") {
+    return;
+  }
+  browser.runtime.sendMessage({"url": e.target.href});
+}
