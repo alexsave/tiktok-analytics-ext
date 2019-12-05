86c913ce682008944b83717d99d27d19edc8ada3
Alex Saveliev <alexvsaveliev@gmail.com>
2019-12-04 20:17:40 -0500

    moved big comment

--- background.js
+++ background.js
@@ -1,42 +1,3 @@
-/*
-    The key intuition here is that by loading the page, TikTok reponds with a bunch of data
-    Reponses matching tiktok.com/share will respond with this format:
-    body:
-        hasMore: true,
-        itemListData: [
-            {
-                authorInfos: {
-                    nickName: "Rocket",
-                    uniqueId: "qzim",
-                    signature: "bio",
-                    userId: "5456789876545678"
-                },
-                challengeInfoList: {
-                    [
-                        {
-                            challengeName:"holidayhacks"
-                        },
-                        ...
-                    ]
-                },
-                itemInfos: {
-                    commentCount: 2,
-                    createTime: 15345678986, 
-                    diggCount: 2,
-                    id: "456787654",
-                    shareCount: 3,
-                    text: "yada yada yada"
-                    video: {urls: ["https://fdsafdsafds]}
-                },
-                musicInfos: {
-                    authorName: "brianna.caston",
-                    musicId: "4568765456",
-                    musicName: "New Hampshire Check"
-                },
-            },
-            ...
-        ]
-*/
 let map = {};
 
 function listener(details) {
@@ -61,12 +22,20 @@ function listener(details) {
     //console.log(obj.body);
     
     const items = obj.body.itemListData;
+    const user = items[0].authorInfos.uniqueId;
+
+    if(!map[user])
+        map[user] = {};
+    
     //each of these items has a itemInfos object that has
     //comment 
-    //also has a autherInfos.uniqueId (ex. qzim) to be safe
-    items.forEach(item => 
-      console.log(item.itemInfos.text + ': ' + item.itemInfos.diggCount)
-    );
+    //also has a authorInfos.uniqueId (ex. qzim) to be safe
+    items.forEach(item => {
+      let tempobject = item.itemInfos.text + ': ' + item.itemInfos.diggCount;
+      //this will overwrite, but that's good as we prevent duplicates
+      map[user][item.itemInfos.id] = tempobject;
+    });
+    console.log(map);
   }
 
   return {};
