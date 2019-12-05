6985e17aed56fab973fc7820af463cbbdab11e84
Alex Saveliev <alexvsaveliev@gmail.com>
2019-12-05 13:12:45 -0500

    popupjs is very small now

--- popup/popup.js
+++ popup/popup.js
@@ -1,24 +1,12 @@
-let username = "";
 const sendMessage = () => {
-  console.log(input.value);
   if (false)
-    browser.runtime.sendMessage({msg: 'start', username: 'qzim'});
+    browser.runtime.sendMessage({msg: 'start', username: input.value});
 };
 
 const input = document.querySelector('input');
 input.onkeyup = e => {
   if(e.keyCode === 13)
-    sendMessage()
+    sendMessage();
 };
-/**
- * Listen for clicks on the buttons, and send the appropriate message to
- * the content script in the page.
- */
-const button = document.querySelector('button');
-button.onclick = () => sendMessage();
-/*document.addEventListener("click", (e) => {
-  //there's literally just one button
-  console.log(input.value);
-  if(false)
-    browser.runtime.sendMessage({msg: 'start', username: 'qzim'});
-});*/
+//Listen for clicks on the buttons, and send the appropriate message to
+document.querySelector('button').onclick = sendMessage;
