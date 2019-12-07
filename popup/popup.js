39a68b13e93ed1c455a0a605d38559ffa9351a05
Alex Saveliev <alexvsaveliev@gmail.com>
2019-12-07 10:23:33 -0500

    can now detect open tiktok page

--- popup/popup.js
+++ popup/popup.js
@@ -10,3 +10,10 @@ input.onkeyup = e => {
 };
 //Listen for clicks on the buttons, and send the appropriate message to
 document.querySelector('button').onclick = sendMessage;
+
+//see if we're already on a tiktok user page that they probably want to scrape
+browser.tabs.query({url: '*://*.tiktok.com/@*'})
+    .then(tabs => {
+        if(tabs.length !== 0)
+            console.log(tabs[0].url);
+    });
